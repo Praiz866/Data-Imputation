299da9593d799024b4eda8e9dc7001eaa3e83d54
Praise Oyesile <praizoyesile@gmail.com>
2025-03-26 13:24:01 +0100

    Update Data Imputation.R

--- Data Imputation.R
+++ Data Imputation.R
@@ -18,25 +18,22 @@ forest_imp <- missForest(airquality)
 mean_imp <- airquality
 mean_imp$Ozone[is.na(mean_imp$Ozone)] <- mean(mean_imp$Ozone, na.rm = TRUE)
 
-# Distribution plots
+# Distribution plots mice
 library(ggplot2)
 ggplot() +
-  geom_density(aes(x = airquality$Ozone), color = "black") +
+  geom_density(aes(x = airquality$Ozone), color = "red") +
   geom_density(aes(x = mice_results$Ozone), color = "blue") +
-  geom_density(aes(x = forest_imp$ximp$Ozone), color = "red")
-
-
-
-
-# Impute data (example with MICE)
-imputed <- mice(airquality, m = 1, method = "pmm")
-complete_data <- complete(imputed)
-
-# Compare densities
-ggplot() +
-  geom_density(aes(x = airquality$Ozone, color = "Original (with NAs)"), fill = NA) +
-  geom_density(aes(x = complete_data$Ozone, color = "MICE-imputed"), fill = NA, linetype = "dashed") +
   labs(title = "Ozone Distribution: Before vs. After Imputation",
        x = "Ozone (ppb)", y = "Density") +
-  scale_color_manual(values = c("red", "blue")) +
   theme_minimal()
+
+# Distribution plots mice  
+library(ggplot2)
+ggplot() +
+  geom_density(aes(x = airquality$Ozone), color = "red") +  
+  geom_density(aes(x = forest_imp$ximp$Ozone), color = "blue") +
+  labs(title = "Ozone Distribution: Before vs. After Imputation",
+       x = "Ozone (ppb)", y = "Density") +
+  theme_minimal()
+
+
